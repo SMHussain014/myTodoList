4306c255892df3d72f775f9cb2206810dcb1bc73
Syed Muhammad Hussain <hussain.sm014@gmail.com>
2024-04-21 01:42:23 +0500

    First Commit

---
+++ public/index.js
@@ -0,0 +1,35 @@
+import inquirer from 'inquirer';
+let todos = [];
+let loop = true;
+while (loop) {
+    const answers = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'TODO',
+            message: 'What do you want to add in your todo?: '
+        },
+        {
+            type: 'confirm',
+            name: 'AddMoreTodo',
+            message: 'Do you want to add more TODOs?: ',
+            default: false
+        }
+    ]);
+    const { TODO, AddMoreTodo } = answers;
+    loop = AddMoreTodo;
+    if (TODO) {
+        todos.push(TODO);
+    }
+    else {
+        console.log('Please enter a valid input:');
+    }
+}
+if (todos.length > 0) {
+    console.log('The desired Todo List is: ');
+    todos.forEach(todo => {
+        console.log(todo);
+    });
+}
+else {
+    console.log('No TODOs found!');
+}
